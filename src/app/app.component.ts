3f76cb9c143bae6121cca1bf50d43e49ab98fac6
Joel Peña Arias <jukey09@gmail.com>
2018-05-15 20:02:32 -0400

    Add list view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import 'node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+@Component({  
+  selector: 'ms-root',  
+  templateUrl: './app.component.html',  
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  pageTitle = 'Management System';
 }
