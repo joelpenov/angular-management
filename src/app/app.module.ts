6164015669ab2cc510641227249f57f7de50e790
Joel Peña Arias <jukey09@gmail.com>
2018-05-16 20:32:32 -0400

    Add service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product_list/product-list.component';
 import { RatingStartsComponent } from './shared/starts.component';
+import { ProductService } from './services/products.service';
   
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { RatingStartsComponent } from './shared/starts.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/starts.component.ts
+++ src/app/shared/starts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, SimpleChanges, Input } from "@angular/core";
+import { Component, OnChanges, SimpleChanges, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector:'ms-starts-rating',
@@ -10,8 +10,12 @@ export class RatingStartsComponent implements OnChanges {
     @Input() startsRating:number=0;
     title:string;
     startsWidth:number=50;
+    @Output() notifier:EventEmitter<string> = new EventEmitter<string>();
     ngOnChanges(changes: SimpleChanges): void {
         this.startsWidth=(this.startsRating * 100)/5;
         this.title = `Rating: ${this.startsRating}`;
+    }
+    onClickStarsHandle=()=> {
+        this.notifier.emit(`The rating with ${this.startsRating } starts was clicked!`)
     }
 }
