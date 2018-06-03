3f44e89040d14e4f4d5830ba38246b7eb7c40d9a
Joel Peña Arias <joelpenov@gmail.com>
2018-06-02 22:03:36 -0400

    Get form values on submit (login)

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import { RouterModule } from '@angular/router';
 import { routes } from './user.routes';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(routes)
   ],
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent
   ]
 })
 export class UserModule { }
--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from "@angular/router";
 import { ProfileComponent } from "./profile/profile.component";
+import { LoginComponent } from "./login/login.component";
 
 export const routes:Routes= [
-    {path: 'profile', component: ProfileComponent}
+    {path: 'profile', component: ProfileComponent},
+    {path: 'login', component: LoginComponent}
 ]
